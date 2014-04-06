f80a945ad50011a3bd45bdc4ab189ead30af3ea1
TheFlyingFiddle <kurtyan@student.chalmers.se>
2014-04-06 20:57:22 +0200

    Better spriter support

--- SpriterTester/state.d
+++ SpriterTester/state.d
@@ -21,7 +21,6 @@ struct SpriterObject
 struct File 
 {
 	string name;
-	int id;
 	int height, width;
 	float2 origin;
 }
@@ -30,20 +29,16 @@ struct Foulder
 {
 	string name;
 	File[] files;
-	int id;
 }
 
 struct Entity
 {
-	int id;
 	string name;
 	Animation[] animations;
 }
 
 struct Ref
 {
-	int id;
-	int parent = -1;
 	int timeline;
 	int key;
 	int z_index;
@@ -51,14 +46,12 @@ struct Ref
 
 struct MainlineKey
 {
-	int id;
 	float time = 0;
 	Ref[] objectRefs;
 }
 
 struct Timeline
 {
-	int id;
 	string name;
 	int objectType;
 	TimelineKey[] keys;
@@ -74,10 +67,10 @@ enum CurveType
 
 struct TimelineKey
 {
-	int id;
-	float time = 0;
+	float time  = 0;
+	float curve0 = 0, curve1 = 0;
 	int curveType = CurveType.linear;
-	float c1 = 0, c2 = 0;
+	int spin		  = 1;
 
 	SpatialInfo info;
 }	
@@ -86,10 +79,10 @@ struct SpatialInfo
 {
 	float2 pos = float2.zero;
 	float2 scale = float2(1,1);
-	float2 origin = float2(0,1);
+	float2 origin = float2(float.nan, float.nan);
 	float rotation = 0;
 	float alpha = 1;
-	int spin = 1;
+
 	int foulder;
 	int file;
 }
@@ -102,7 +95,6 @@ enum LoopType
 
 struct Animation
 {
-	int id;
 	float length;
 	int looptype = LoopType.looping;
 	string name;
@@ -117,12 +109,11 @@ auto readFile(JSONValue[string] file)
 	File f;
 	foreach(k, v; file) switch(k)
 	{
-		case "id":	   f.id			 = cast(int)v.integer;   break;
 		case "height"  : f.height	 = cast(int)v.integer;   break;
 		case "width"   : f.width	 = cast(int)v.integer;   break;
 		case "name"    : f.name		 = v.str;				    break;
 		case "pivot_x" : f.origin.x = cast(float)v.floatOrInt; break;
-		case "pivot_y" : f.origin.y = cast(float)v.floatOrInt; break;
+		case "pivot_y" : f.origin.y = v.floatOrInt; break;
 
 	   default: break;
 	}
@@ -143,7 +134,6 @@ auto readFoulder(JSONValue[string] foulder)
 	Foulder f;
 	foreach(k, v; foulder) switch(k)
 	{
-		case "id":	 f.id = cast(int)v.integer; break;
 		case "file": f.files = readFiles(v.array); break;
 		case "name": f.name = v.str; break;
 		default: break;
@@ -157,8 +147,6 @@ auto readObjectRef(JSONValue[string] ref_)
 	Ref r;
 	foreach(k, v; ref_) switch(k)
 	{
-		case "id":			r.id			= cast(int)v.integer; break;
-		case "parent":		r.parent		= cast(int)v.integer; break;
 		case "key":			r.key			= cast(int)v.integer; break;
 		case "timeline":	r.timeline	= cast(int)v.integer; break;
 		case "z_index":	r.z_index	= cast(int)v.integer; break;
@@ -181,7 +169,6 @@ auto readMainKey(JSONValue[string] key)
 	{
 		switch(k)
 		{
-			case "id":	 m.id   = cast(int)v.integer; break;
 			case "time": m.time = cast(int)v.integer / 1000f; break;
 			case "object_ref": m.objectRefs = readObjectRefs(v.array);	break;
 			default: break;
@@ -236,10 +223,30 @@ auto readTKey(JSONValue[string] tKey)
 	TimelineKey t;
 	foreach(k, v; tKey) switch(k)
 	{
-		case "id":	   t.id			= cast(int)v.integer;			 break;
 		case "object": t.info		= readSpatial(v.object);		 break;
-		case "spin"  : t.info.spin = cast(int)v.integer;			 break;
+		case "spin"  : t.spin		= cast(int)v.integer;			 break;
 		case "time"  : t.time      = cast(int)v.integer / 1000f;	 break;
+		case "c0"	 : t.curve0    = v.floatOrInt();					 break;
+		case "c1"    : t.curve1    = v.floatOrInt();					 break;
+		case "curve_type" : 
+			writeln("CurveType ", v.type);
+			switch(v.str)
+			{
+				case "instant":
+					t.curveType = CurveType.instant;
+					break;
+				case "linear":
+					t.curveType = CurveType.linear;
+					break;
+				case "quadratic":
+					t.curveType = CurveType.quadratic;
+					break;
+				case "cubic":
+					t.curveType = CurveType.cubic;
+					break;
+				default: assert(0, "Invalid curve type");
+			}
+		break;
 
 		default: break;
 	}
@@ -261,7 +268,6 @@ auto readTimeline(JSONValue[string] timelines)
 	Timeline t;
 	foreach(k, v; timelines) switch(k)
 	{
-		case "id":	 t.id   = cast(int)v.integer; break;
 		case "name": t.name = v.str; break;
 		case "key":  t.keys = readTKeys(v.array);	break;
 		default: break;
@@ -284,7 +290,6 @@ auto readAnimation(JSONValue[string] animation)
 	{
 		switch(k)
 		{
-			case "id":		  a.id = cast(int)v.integer; break;
 			case "length":   a.length = v.integer / 1000f; break;
 			case "mainline": a.mainlines = readMainlines(v.object); break;
 			case "timeline": a.timelines = readTimelines(v.array); break;
@@ -310,7 +315,6 @@ auto readEntity(JSONValue[string] entity)
 	{
 		switch(k)
 		{
-			case "id":			 e.id   = cast(int)v.integer; break;
 			case "name":		 e.name = v.str; break;
 			case "animation":  e.animations = readAnimations(v.array);	break;
 			default: break;
@@ -349,13 +353,25 @@ class TestState : IGameState
 			}
 		}
 
+		foreach(entity; object.entities)
+		foreach(animation; entity.animations)
+		foreach(timeline; animation.timelines)
+		foreach(ref key; timeline.keys)
+		{
+			if(std.math.isNaN(key.info.origin.x) != 0)
+			{
+				float2 origin = object.foulders[key.info.foulder].files[key.info.file].origin;
+				key.info.origin = origin;
+			}
+		}
+
 	}
 
 	void enter()  { }
 	void exit()   { }
 	void update() 
 	{ 
-		object.currentTime += Time.delta;
+		object.currentTime += Time.delta * 0.3;
 		if(object.currentTime > object.entities[0].animations[0].length &&
 			object.entities[0].animations[0].looptype == LoopType.looping)
 			object.currentTime = 0;
@@ -370,56 +386,111 @@ class TestState : IGameState
 		 {
 			 foreach(a; e.animations)
 			 {
-				 foreach(o; a.mainlines[0].objectRefs)
-				 {
-				    writeln(a.timelines[o.timeline].name);
 
-					 SpatialInfo spatial =  a.timelines[o.timeline].info(object.currentTime);
+				auto mainline = a.mainlines.line(object.currentTime);
+
+				 foreach(o; mainline.objectRefs)
+				 {
+					 SpatialInfo spatial =  a.timelines[o.timeline].info(o.key, a.looptype == 1, object.currentTime, a.length);
 					 writeln("Loading: ", object.foulders[spatial.foulder].files[spatial.file].name);
 
 					 auto frame   =  
 						 Frame(Game.content.loadTexture(
 								 object.foulders[spatial.foulder].files[spatial.file].name));
 			
-					 Game.renderer.addFrame(frame, 
-												  float2(Game.window.size / 2) + spatial.pos, 
-												  Color.white, 
-												  spatial.scale, 
-												  spatial.origin * float2(frame.srcRect.z, frame.srcRect.w) * spatial.scale,
-												  spatial.rotation);
+					 foreach(i; 4 .. 14)
+					 {
+
+						 Game.renderer.addFrame(frame, 
+														float2(100, 40) * i + spatial.pos, 
+														Color.white, 
+														spatial.scale, 
+														spatial.origin * float2(frame.srcRect.z, frame.srcRect.w),
+														spatial.rotation);
+					 }
 				 }
 			 }
 		 }
 	}
 }
 
-SpatialInfo info(Timeline timeline, bool looping, float currentTime)
+auto line(MainlineKey[] lines, float currentTime)
 {
 	int index = -1;
-	foreach(i; 1 .. timeline.keys.length)
+	foreach(i; 1 .. lines.length)
 	{
-		if(timeline.keys[i].time > currentTime)
+		if(lines[i].time > currentTime)
 		{
 			index = i;
-			break;
+			return lines[index - 1];
 		}
 	}
+	
+	return lines[$ - 1];
+}
 
-	if(index == -1) 
+SpatialInfo info(Timeline timeline, int key, bool looping, float currentTime, float totalTime)
+{
+	if(key == timeline.keys.length - 1) 
 	{
-		return timeline.keys[$ - 1].info;
+		if(looping)
+		{
+			float linearTime = (currentTime - timeline.keys[$ - 1].time) /
+								    (totalTime   - timeline.keys[$ - 1].time);
+
+			float time = curveTime(timeline.keys[$ - 1], linearTime);
+			return linear(timeline.keys[$ - 1].info, 
+							  timeline.keys[0].info,
+							  timeline.keys[$ - 1].spin, 
+							  time);
+
+		} else {
+			return timeline.keys[$ - 1].info;
+		}
 	} 
 	else 
 	{
-		float time = (currentTime - timeline.keys[index - 1].time) /
-						 (timeline.keys[index].time - timeline.keys[index - 1].time);
-		return linear(timeline.keys[index - 1].info, timeline.keys[index].info, time);
+		float linearTime = (currentTime - timeline.keys[key].time) / 
+			                (timeline.keys[key + 1].time - timeline.keys[key].time);
+
+		float time = curveTime(timeline.keys[key], linearTime);
+		return linear(timeline.keys[key].info, 
+						  timeline.keys[key + 1].info, 
+						  timeline.keys[key].spin, time);
+	}
+}
+
+float curveTime(ref TimelineKey key0, float t)
+{
+	final switch(key0.curveType)
+	{
+		case CurveType.instant:
+			return 0;
+			break;
+		case CurveType.linear:
+			return t;
+			break;
+		case CurveType.quadratic:
+			return quadratic(0.0, key0.curve0, 1.0, t);
+			break;
+		case CurveType.cubic:
+			return cubic(0.0, key0.curve0, key0.curve1, 1.0, t);
 	}
 }
 
 float linear(float a, float b, float t)
 {
 	return (b - a) * t + a;
+}
+
+float quadratic(float a, float b, float c, float t)
+{
+	return linear(linear(a,b,t), linear(b,c,t), t);
+}
+
+float cubic(float a, float b, float c, float d, float t)
+{
+	return linear(quadratic(a,b,c,t), quadratic(b,c,d,t), t);
 }
 
 float rotationLinear(float rot0, float rot1, int spin, float t)
@@ -441,14 +512,14 @@ float rotationLinear(float rot0, float rot1, int spin, float t)
 	return linear(rot0, rot1, t);
 }
 
-SpatialInfo linear(SpatialInfo info0, SpatialInfo info1, float t)
+SpatialInfo linear(SpatialInfo info0, SpatialInfo info1, int spin, float t)
 {
 	SpatialInfo result;
 	result.pos.x			= linear(info0.pos.x, info1.pos.x, t);
 	result.pos.y			= linear(info0.pos.y, info1.pos.y, t);
 	result.scale.x			= linear(info0.scale.x, info1.scale.x, t);
 	result.scale.y			= linear(info0.scale.y, info1.scale.y, t);
-	result.rotation		= rotationLinear(info0.rotation, info1.rotation, info0.spin, t);
+	result.rotation		= rotationLinear(info0.rotation, info1.rotation, spin, t);
 	result.origin			= info0.origin;
 	result.file				= info0.file;
 	result.foulder			= info0.foulder;
